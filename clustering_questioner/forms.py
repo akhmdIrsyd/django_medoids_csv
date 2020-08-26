d22ae3dd8a40426c89337bd727c0d5275fe59072
akhmad Irsyad <blank.irsyad@gmail.com>
2020-08-26 12:21:38 +0700

    revisi

--- clustering_questioner/forms.py
+++ clustering_questioner/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from bootstrap_modal_forms.forms import BSModalForm
 from .models import upload_data
 
 
--- clustering_questioner/views.py
+++ clustering_questioner/views.py
@@ -19,6 +19,16 @@ from .models import upload_data
 import os
 from django.conf import settings
 
+def likert2target(internet):
+    return {
+        'sangat setuju': 10,
+        'setuju': 8,
+        'netral' : 6,
+        'tidak setuju': 4,
+        'sangat tidak setuju' : 2
+    }[internet]
+
+
 
 def index2(request):
     Upload_data = upload_data.objects.filter(id=108).values_list('isi')[0]
@@ -26,16 +36,47 @@ def index2(request):
     dat = open(os.path.join(settings.MEDIA_ROOT, Upload_data))
     df = pd.read_csv(dat, delimiter=';', encoding='utf-8')
     df = df.dropna()
-    df.columns = ['Date', 'nama', 'kelamin', 'asal', 'jurusan', 'semester', 'info', 'dorogan', 'alasan', 'pendapat',
-                  'peluang']
-    
+    df.columns = ['Date', 'nama', 'jurusan', 'semester', 'internet', 'alumni', 'media', 'beasiswa','kerja',
+                  'belajar']
+    df_semester = df.groupby(['semester']).size().reset_index(name='value')
+    nama_semester=df_semester.semester.tolist()
+    jml_semester=df_semester.value.tolist()
     df_jurusan = df.groupby(['jurusan']).size().reset_index(name='value')
     nama_jurusan=df_jurusan.jurusan.tolist()
     jml_jurusan=df_jurusan.value.tolist()
+    IT=df_jurusan.loc[df_jurusan['jurusan']=='Informatika']
+    if IT.empty:
+        jml_it=0
+    else:
+        jml_it=IT.iloc[0]['value']
+        pass
+    
+    sipil=df_jurusan.loc[df_jurusan['jurusan']=='Sipil']
+    if sipil.empty:
+        jml_sipil=0
+    else:
+        jml_sipil=sipil.iloc[0]['value']
+    
+    arsitek=df_jurusan.loc[df_jurusan['jurusan']=='Arsitek']
+    if arsitek.empty:
+        jml_arsitek=0
+    else:
+        jml_arsitek=arsitek.iloc[0]['value']
+    
+    elektro=df_jurusan.loc[df_jurusan['jurusan']=='Elektro']
+    if elektro.empty:
+        jml_elektro=0
+    else:
+        jml_elektro=elektro.iloc[0]['value']
+    
+    mesin=df_jurusan.loc[df_jurusan['jurusan']=='Mesin']
+    if mesin.empty:
+        jml_mesin=0
+    else:
+        jml_mesin=mesin.iloc[0]['value']
+    
 
-    df_JK = df.groupby(['kelamin']).size().reset_index(name='value')
-    nama_JK = df_JK.kelamin.tolist()
-    jml_JK = df_JK.value.tolist()
+    
     le = LabelEncoder()
     df_encoded = df.apply(le.fit_transform)
     df_encoded
@@ -43,15 +84,29 @@ def index2(request):
 
     k = 3
     model = k_medoids(k)
+    df_count = len(df.index)
     # print('Centers found by your model:')
     
     model.fit(X)
     m = model.fit(X)
+
+    df_semester = df.groupby(['semester']).size().reset_index(name='value')
+    nama_semester = df_semester.semester.tolist()
+    jml_semester = df_semester.value.tolist()
     context={
-        'nama_JK':nama_JK,
-        'jml_JK': jml_JK,
         'jml_jurusan':jml_jurusan,
-        'nama_jurusan':nama_jurusan
+        'nama_jurusan':nama_jurusan,
+        'jml_semester':jml_semester,
+        'nama_semester':nama_semester,
+        'jml_it':jml_it,
+        'jml_sipil':jml_sipil,
+        'jml_mesin':jml_mesin,
+        'jml_eletro':jml_elektro,
+        'jml_arsitek':jml_arsitek,
+        'total':df_count,
+        'nama_semester':nama_semester,
+        'jml_semester': jml_semester,
+
     }
     return render(request, 'home2.html', context )
 
@@ -74,7 +129,7 @@ def update(request, id):
         form = Upload_dataForm(request.POST, request.FILES, instance=upload_datas)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('index2')
     else:
         form = Upload_dataForm(instance=upload_datas)
         return render(request, 'upload_form.html', {'form': form})
@@ -88,8 +143,8 @@ def data_list(request):
     dat = open(os.path.join(settings.MEDIA_ROOT, Upload_data1))
     df = pd.read_csv(dat, delimiter=';', encoding='utf-8')
     df = df.dropna()
-    df.columns = ['Date', 'nama', 'kelamin', 'asal', 'jurusan', 'semester', 'info', 'dorogan', 'alasan', 'pendapat',
-                  'peluang']
+    df.columns = ['Date', 'nama', 'jurusan', 'semester', 'internet', 'alumni', 'media', 'beasiswa','kerja',
+                  'belajar']
     context = {'columns': df.columns, 'rows': df.to_dict('records'), 'Upload_data': Upload_data}
     return render(request, 'data.html', context)
 
@@ -100,14 +155,21 @@ def cluster_list(request):
     dat = open(os.path.join(settings.MEDIA_ROOT, Upload_data))
     df = pd.read_csv(dat, delimiter=';', encoding='utf-8')
     df = df.dropna()
-    df.columns = ['Date', 'nama', 'kelamin', 'asal', 'jurusan', 'semester', 'info', 'dorogan', 'alasan', 'pendapat',
-                  'peluang']
-
-    le = LabelEncoder()
-    df_encoded = df.apply(le.fit_transform)
-    df_encoded
-    X = np.array(df_encoded)
-
+    df.columns = ['Date', 'nama', 'jurusan', 'semester', 'internet', 'alumni', 'media', 'beasiswa','kerja',
+                  'belajar']
+    df=df.apply(lambda x: x.astype(str).str.lower())
+    #le = LabelEncoder()
+    #df_encoded = df.apply(le.fit_transform)
+    #df_encoded
+    internet = df.internet.apply(likert2target)
+    alumni = df.alumni.apply(likert2target)
+    media = df.media.apply(likert2target)
+    beasiswa = df.beasiswa.apply(likert2target)
+    kerja = df.kerja.apply(likert2target)
+    belajar = df.belajar.apply(likert2target)
+    #X = np.array(df_encoded)
+    df_en=pd.DataFrame(zip(internet, alumni, media,beasiswa,kerja,belajar))
+    X = np.array(df_en)
     k = 3
     model = k_medoids(k)
     # print('Centers found by your model:')
@@ -161,22 +223,34 @@ def gambar(X, label):
     plt.show()
 
 def visual(request):
+    #baca data
     Upload_data = upload_data.objects.filter(id=108).values_list('isi')[0]
     Upload_data = Upload_data[0]
     dat = open(os.path.join(settings.MEDIA_ROOT, Upload_data))
     df = pd.read_csv(dat, delimiter=';', encoding='utf-8')
     df = df.dropna()
-    df.columns = ['Date', 'nama', 'kelamin', 'asal', 'jurusan', 'semester', 'info', 'dorogan', 'alasan', 'pendapat',
-                  'peluang']
-    list(df.columns.values)
-    sentiment_counts = df.jurusan.value_counts()
-
+    df.columns = ['Date', 'nama', 'jurusan', 'semester', 'internet', 'alumni', 'media', 'beasiswa','kerja',
+                  'belajar']
+    df=df.apply(lambda x: x.astype(str).str.lower())
+    #proses encode
     le = LabelEncoder()
     df_encoded = df.apply(le.fit_transform)
     df_encoded
+    nama= df_encoded.nama
+    jurusan=df_encoded.jurusan
+    semester=df_encoded.semester
+    internet = df.internet.apply(likert2target)
+    alumni = df.alumni.apply(likert2target)
+    media = df.media.apply(likert2target)
+    beasiswa = df.beasiswa.apply(likert2target)
+    kerja = df.kerja.apply(likert2target)
+    belajar = df.belajar.apply(likert2target)
+    ind=df.index
     X = np.array(df_encoded)
-
-    k = 3
+    df_en=pd.DataFrame(zip(nama,jurusan,semester,internet, alumni, media,beasiswa,kerja,belajar))
+    X = np.array(X)
+    #kmedoids
+    k = 4
     model = k_medoids(k)
     # print('Centers found by your model:')
     model.fit(X)
@@ -189,7 +263,13 @@ def visual(request):
     mahasiswa = df.values.tolist()
     
     
-
+    #u=[]
+    #dd1f.columns = ['ind','inde','a','b','c','d','e','f','g','h','i']
+    #for j in dd1f.inde:
+    #    for i in df.index:
+    #        if  i==j:
+    #            u.append(df.nama[i])
+    #dd1f['nama']=u
     dd1f = pd.DataFrame(m).reset_index()
 
 
@@ -208,6 +288,7 @@ def visual(request):
         'cluster' : pred,
         'hello' : 'hello world',
         'columns': df_decoded.columns,
-        'rows': df_decoded.to_dict('records')
+        'rows': df_decoded.to_dict('records'),
+        #'u':u
     }
     return render(request, 'visual.html', context)
